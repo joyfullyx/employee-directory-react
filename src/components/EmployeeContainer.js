ed91b8af426481d90681bbb498c97a0d49cce393
Joy Rhee <joyfullyx@gmail.com>
2021-05-19 10:33:19 -0700

    push current to heroku

--- src/components/EmployeeContainer.js
+++ src/components/EmployeeContainer.js
@@ -1,65 +1,64 @@
-import React from 'react'
-import API from '../utils/API';
-import Header from './Header';
-import ResultsList from './ResultsList';
-import SearchForm from './SearchForm';
+import React from "react";
+import API from "../utils/API";
+import Header from "./Header";
+import ResultsList from "./ResultsList";
+import SearchForm from "./SearchForm";
 
 class EmployeeContainer extends React.Component {
-    state = {
-        result: {},
-        search: "",
-        employees: [],
-        employeesFiltered: [],
-        // TODO: add name/dob in asc/desc order here??
-    };
+  state = {
+    search: "",
+    employees: [],
+    employeesFiltered: [],
+    // TODO: add name/dob in asc/desc order here??
+  };
 
-    // default on reload
-    // TODO: ??????????????????????
-    componentDidMount() {
-        API.search()
-            .then((res) => {
-                console.log(res)
-                this.setState({
-                    employees: res.data.results
-                })
-            })
-            .catch(err => console.log(err))
-    }
-
-    searchEmployee = query => {
-        API.search(query)
-            .then(res => this.setState({ result: res.data }))
-            .catch(err => console.log(err))
-    };
-
-    handleInputChange = event => {
-        const value = event.target.value;
-        const name = event.target.name;
+  // default on reload
+  // TODO: ??????????????????????
+  componentDidMount() {
+    API.search()
+      .then((res) => {
+        console.log(res);
         this.setState({
-            [name]: value
+          employees: res.data.results,
+          // employeesFiltered: res.data.results
         });
-    };
+      })
+      .catch((err) => console.log(err));
+  }
 
-    handleFormSubmit = event => {
-        event.preventDefault();
-        this.searchEmployee(this.state.search)
-    };
+  searchEmployee = (query) => {
+    API.search(query)
+      .then((res) => this.setState({ result: res.data }))
+      .catch((err) => console.log(err));
+  };
 
-    render() {
-        return (
-            <div>
-                <Header />
-                <SearchForm />
-                <ResultsList 
-                picture={this.state.result.picture}
-                name={this.state.result.name}
-                phone={this.state.result.phone}
-                email={this.state.result.email}
-                dob={this.state.result.dob}
-                />
-            </div>
-        )
-    }
+  handleInputChange = (event) => {
+    const value = event.target.value;
+    const results = event.target.results;
+    this.setState({
+      search: value,
+      employeesFiltered: results,
+    });
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    this.searchEmployee(this.state.search);
+  };
+
+  render() {
+
+    return (
+      <div>
+        <Header />
+        <SearchForm
+          value={this.state.search}
+          handleInputChange={this.handleInputChange}
+        />
+        <ResultsList />
+      </div>
+    );
+  }
 }
 
 export default EmployeeContainer;
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASEURL = "https://randomuser.me/api/?results=100&nat=us&inc=picture,name,phone,email,dob";
+// const BASEURL = "https://randomuser.me/api/?results=100&nat=us&inc=picture,name,phone,email,dob";
+const BASEURL = "https://randomuser.me/api/?results=100&nat=us"
 
 // export default {
 //   search: function(query) {
