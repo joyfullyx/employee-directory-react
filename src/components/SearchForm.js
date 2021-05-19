1c331fa2716a119a59ceeb02652e78e7af3caadd
Joy Rhee <joyfullyx@gmail.com>
2021-05-18 23:41:12 -0700

    search button added in input field

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,16 +3,25 @@ import "../styles/SearchForm.css";
 
 function SearchForm(props) {
   return (
-    <form className="col-sm-12 p-3">
+    <form className="input-group mb-3 col-sm-12 p-3">
       <input
         onChange={props.handleInputChange}
         value={props.value}
-        type="text"
         name="search"
+        type="text"
         className="form-control"
         placeholder="search"
         id="search"
       ></input>
+
+      <button
+        onClick={props.handleFormSubmit}
+        className="btn btn-outline-secondary"
+        type="button"
+        id="button-addon2"
+      >
+        Search now!
+      </button>
     </form>
   );
 }
